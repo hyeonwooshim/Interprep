4305aa49e532320bae868e8ab7be3840cf2ab59c
Eric Shim <eric.shim@li.me>
2020-03-21 19:54:36 -0400

    Paste in sample web app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,174 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: SignUpScreen(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SignUpScreen extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 400,
+        child: Card(
+          child: SignUpForm(),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _SignUpFormState extends State<SignUpForm>
+    with SingleTickerProviderStateMixin {
+  // STEP 3: Add an AnimationController and add the
+  // AnimatedBuilder with a LinearProgressIndicator to the Column
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SignUpFormBody(
+          onProgressChanged: (progress) {
+            // STEP 2: Create a _formCompleted field on this class
+            // and set it here when the progress is 1
+          },
         ),
+        Container(
+          height: 40,
+          width: double.infinity,
+          margin: EdgeInsets.all(12),
+          child: FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            // STEP 1: Add a callback here and navigate to
+            // the welcome screen when the button is tapped.
+            onPressed: null,
+            child: Text('Sign up'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SignUpFormBody extends StatefulWidget {
+  final ValueChanged<double> onProgressChanged;
+
+  SignUpFormBody({
+    @required this.onProgressChanged,
+  }) : assert(onProgressChanged != null);
+
+  @override
+  _SignUpFormBodyState createState() => _SignUpFormBodyState();
+}
+
+class _SignUpFormBodyState extends State<SignUpFormBody> {
+  static const EdgeInsets padding = EdgeInsets.all(8);
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController websiteController = TextEditingController();
+
+  List<TextEditingController> get controllers =>
+      [emailController, phoneController, websiteController];
+
+  @override
+  void initState() {
+    super.initState();
+    controllers.forEach((c) => c.addListener(() => _updateProgress()));
+  }
+
+  double get _formProgress {
+    var progress = 0.0;
+    for (var controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+    return progress;
+  }
+
+  void _updateProgress() {
+    widget.onProgressChanged(_formProgress);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: padding,
+            child: Text('Sign up', style: Theme.of(context).textTheme.display1),
+          ),
+          SignUpField(
+            hintText: 'E-mail address',
+            controller: emailController,
+          ),
+          SignUpField(
+            hintText: 'Phone number',
+            controller: phoneController,
+          ),
+          SignUpField(
+            hintText: 'Website',
+            controller: websiteController,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SignUpField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+
+  SignUpField({
+    @required this.hintText,
+    @required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        decoration: InputDecoration(hintText: hintText),
+        controller: controller,
+      ),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome!', style: Theme.of(context).textTheme.display3),
+      ),
     );
   }
 }
