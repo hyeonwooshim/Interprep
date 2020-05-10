a0564ea0dde3e5a07c77969e6d69bea046586f56
Eric Shim <eric.shim@li.me>
2020-05-10 15:06:24 -0400

    Finish implementing TwoLineFormat.

--- lib/services/formatter/passage_format.dart
+++ lib/services/formatter/passage_format.dart
@@ -2,5 +2,5 @@ import 'package:interprep/services/bible/passage.dart';
 
 abstract class PassageFormat {
   /// Returns a formatted String based on [passage].
-  String formatPassage(Passage passage1, Passage passage2);
+  String formatPassagePair(Passage passage1, Passage passage2);
 }
--- lib/services/formatter/two_line_format.dart
+++ lib/services/formatter/two_line_format.dart
@@ -2,11 +2,33 @@ import 'package:interprep/services/bible/passage.dart';
 import 'package:interprep/services/formatter/passage_format.dart';
 
 class TwoLineFormat extends PassageFormat {
-  bool locationBefore;
+  String formatPassagePair(Passage passage1, Passage passage2,
+      {locationFirst = true,
+      showVerseNums = false,
+      useAbbreviation1 = false,
+      useAbbreviation2 = false}) {
+    final formatted1 = _formatIndividualPassage(
+        passage1, locationFirst, useAbbreviation1, showVerseNums);
+    final formatted2 = _formatIndividualPassage(
+        passage2, locationFirst, useAbbreviation2, showVerseNums);
 
-  TwoLineFormat(locationBefore);
+    return '$formatted1\n$formatted2';
+  }
 
-  String formatPassage(Passage passage1, Passage passage2) {
-    return '';
+  String _formatIndividualPassage(
+      passage, locationFirst, useAbbreviation, showVerseNums) {
+    var versesStr;
+    if (showVerseNums) {
+      versesStr = passage.verses.map((v) => '${v.verse}. ${v.text}').join(' ');
+    } else {
+      versesStr = passage.verses.map((v) => '${v.text}').join(' ');
+    }
+
+    final loc = passage.locationString(useAbbreviation: useAbbreviation);
+    if (locationFirst) {
+      return '($loc) $versesStr';
+    } else {
+      return '$versesStr ($loc)';
+    }
   }
 }
