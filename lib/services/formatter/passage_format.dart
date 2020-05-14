434de82607e1c68816479d69f7d2e12bc32a55e8
Eric Shim <eric.shim@li.me>
2020-05-14 12:07:20 -0400

    Create two column format for read verses (#9)

--- lib/services/formatter/passage_format.dart
+++ lib/services/formatter/passage_format.dart
@@ -1,6 +1,6 @@
 import 'package:interprep/services/bible/passage.dart';
 
 abstract class PassageFormat {
-  /// Returns a formatted String based on [passage].
+  /// Returns a formatted String based on [passage1] and [passage2].
   String formatPassagePair(Passage passage1, Passage passage2);
 }
---
+++ lib/services/formatter/two_column_format.dart
@@ -0,0 +1,25 @@
+import 'package:interprep/services/bible/passage.dart';
+import 'package:interprep/services/formatter/passage_format.dart';
+
+class TwoColumnFormat extends PassageFormat {
+  String formatPassagePair(Passage passage1, Passage passage2,
+      {useAbbreviation1 = false, useAbbreviation2 = false}) {
+    final loc1 = passage1.locationString(useAbbreviation: useAbbreviation1);
+    final loc2 = passage2.locationString(useAbbreviation: useAbbreviation2);
+
+    final formatted1 = _formatIndividualPassage(passage1);
+    final formatted2 = _formatIndividualPassage(passage2);
+
+    return '<pre><b>"$loc1"&#9;"$loc2"</b></pre><br>'
+        '<table style="border:1px solid black; border-collapse:collapse;'
+        'padding-left:0.08in; padding-right:0.08in; padding-top:0in; padding-bottom:0in;">'
+        '<tr>$formatted1$formatted2</tr>'
+        '</table>';
+  }
+
+  String _formatIndividualPassage(passage) {
+    final formatted =
+        passage.verses.map((v) => '${v.verse}. ${v.text}').join('<br><br>');
+    return '<td style="vertical-align:top; border:1px solid black;">$formatted</td>';
+  }
+}
